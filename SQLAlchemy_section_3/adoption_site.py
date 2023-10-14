5527d0d2ad085528386eecfaf6ec2ea27f1d1593
everprogrammer <amir.tavakolian68@gmail.com>
2023-10-14 10:24:05 +0330

    Create Add Owner tab to navbar
    - create AddOwner in forms.py
    - create add_owner view in adoption_site
    - create HTML file for adding new owner

--- SQLAlchemy_section_3/adoption_site.py
+++ SQLAlchemy_section_3/adoption_site.py
@@ -1,5 +1,5 @@
 import os
-from forms import AddForm, DelForm
+from forms import AddForm, DelForm, AddOwner
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -25,15 +25,34 @@ class Puppy(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}"
+        if self.owner:
+            return f"Puppy name: {self.name} and its owner is {self.owner}"
+        return f"Puppy name: {self.name} and does not have owner yet!"
 
+    
+class Owner(db.Model):
 
-########## Views ####
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner name: {self.name}"
+
+########## Views ####   
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -74,6 +93,22 @@ def del_pup():
 
         return redirect(url_for('list_pup'))
     return render_template('delete.html', form=form)
+
+
+@app.route('/owner', methods=['GET', 'POST'])
+def add_owner():
+
+    form = AddOwner()
+
+    if form.validate_on_submit():
+        owner_name = form.name.data
+        pup_id = form.puppy_id.data
+        owner = Owner(owner_name, pup_id)
+        db.session.add(owner)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('owner.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- SQLAlchemy_section_3/forms.py
+++ SQLAlchemy_section_3/forms.py
@@ -12,4 +12,10 @@ class DelForm(FlaskForm):
     id = IntegerField('Id Number of puppy to remove:')
     submit = SubmitField('Remove Puppy')
 
+class AddOwner(FlaskForm):
+
+    name = StringField('Please enter name of the owner:')
+    puppy_id = IntegerField('Please enter the id of the puppy:')
+    submit = SubmitField('Add Owner')
+    
     
