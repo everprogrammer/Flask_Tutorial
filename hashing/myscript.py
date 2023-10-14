c73e2a6db7d2515b5f9ba777366ffcfd09432eb6
everprogrammer <amir.tavakolian68@gmail.com>
2023-10-14 16:43:27 +0330

    - Add authenticate to backend

---
+++ hashing/myscript.py
@@ -0,0 +1,27 @@
+# from flask_bcrypt import Bcrypt
+
+# bcrypt = Bcrypt()
+
+# my_pass = 'supersecretpassword'
+
+
+# print(hashed_pass)
+
+# check_1 = bcrypt.check_password_hash(hashed_pass, 'wrongpassword') 
+# print(check_1)
+
+# check_2 = bcrypt.check_password_hash(hashed_pass, 'supersecretpassword')
+# print(check_2)
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+my_pass = 'supersecretpassword'
+
+hashed_pass = generate_password_hash(my_pass)
+print(hashed_pass)
+
+check_1 = check_password_hash(hashed_pass, 'wrongpassword') 
+print(check_1)
+
+check_2 = check_password_hash(hashed_pass, 'supersecretpassword')
+print(check_2)
